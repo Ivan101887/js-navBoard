e54a5a319b3761ccde6c479f6b26e380fb778358
Ivan yuan <Ivan_Yuan@cmoney.com.tw>
2022-02-18 15:21:31 +0800

    'version-3'

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 const elemNav = document.querySelector('#Nav');
 setEvent();
 function setEvent() {
-  elemNav.addEventListener('mouseover', atEnter, true);
-  elemNav.addEventListener('mouseout', cancelEnter, true);
+  elemNav.addEventListener('mouseover', atEnter, false);
+  elemNav.addEventListener('mouseout', cancelEnter);
 }
+
 function atEnter(e) {
   const self = e.target;
   const next = self.nextElementSibling;
-  console.log(self)
-  if (next === null || next.nodeName !== 'UL') return;
+  console.log(e);
+  e.preventDefault();
   next.style.display = 'block';
+
+  // console.log(next);
+  // e.stopPropagation();
+  // if (next === null || next.nodeName !== 'UL') return;
+  // console.log(666)
+
 }
+
 function cancelEnter(e) {
   
 }
